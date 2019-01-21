1c5d8114fc7a97d7ff75a12067eb20325177d756
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-01-21 15:27:28 +0300

    adding the whole

--- lib/event.rb
+++ lib/event.rb
@@ -1,3 +1,24 @@
-class Event
+require "time"
 
+class Event
+	attr_accessor :start_date, :length, :title, :attendees
+	def initialize(t, l, ttl, att)
+		@start_date = Time.now
+		@start_date = t
+		@length = l
+		@title = ttl
+		@attendees = att 
+	end
+	def postpone_24(t, l, ttl, att)
+		@start_date = Time.now + 24 * 60 * 60
+		@start_date = t
+		@length = l
+		@title = ttl
+		@attendees = att
+	end
 end
+new_event = Event.new("2019-01-13 09:00", 10, "standup quotidien", ["truc@machin.com", "bidule@chose.fr"])
+puts new_event.start_date
+puts new_event.length
+puts new_event.title
+puts new_event.attendees
