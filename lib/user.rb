4aec52618997aab9b70cb728ff591a13996f7464
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-01-21 13:48:20 +0300

    adding the user.rb

--- lib/user.rb
+++ lib/user.rb
@@ -1,3 +1,22 @@
 class User
-
+	attr_accessor :email, :name
+	@@count = 0
+	# creation of initialize
+	def initialize(my_email)
+		@email = my_email
+		return my_email
+	end
+	# creation of the name
+	def name(my_name)
+		@name = my_name
+		return my_name
+	end
+	# creation of the count
+	def self.count
+		@@count += 1
+		return @@count
+	end
 end
+user = User.new("example@example.com")
+user.name("Rakoto")
+User.count
